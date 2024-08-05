c670d1753e76dc775b8701cd6a9168014dda6e15
“gustavobr1001” <100322388+gustavobraga1001@users.noreply.github.com>
2024-08-05 18:52:45 -0300

    Feature: update styles component door step #05

--- src/components/Door.jsx
+++ src/components/Door.jsx
@@ -6,6 +6,7 @@ const Door = (props) => {
       <div className={styles.frame}>
         <div className={styles.door}>
           <div className={styles.number}>3</div>
+          <div className={styles.handle}></div>
         </div>
       </div>
       <div className={styles.bottom}> 
