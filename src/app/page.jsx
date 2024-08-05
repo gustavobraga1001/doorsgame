6406125838b3af91c25821be559fb131d0e714dc
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-08-05 14:57:43 -0300

    first commit

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import Present from "@/components/Present";
+
+export default function Home() {
+  return (
+    <main>
+      <div>
+        <Present />
+      </div>
+    </main>
+  );
+}
---
+++ src/components/Present.jsx
@@ -0,0 +1,14 @@
+import styles from "../styles/Present.module.css";
+
+const Present = () => {
+  return (
+    <div className={styles.present}>
+      <div className={styles.top}></div>
+      <div className={styles.body}></div>
+      <div className={styles.tape1}></div>
+      <div className={styles.tape2}></div>
+    </div>
+  );
+};
+
+export default Present;
