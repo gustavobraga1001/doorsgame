da8283b6e3b13570bcaa1b16dae31d34c08a236e
gustavobraga1001 <100322388+gustavobraga1001@users.noreply.github.com>
2024-08-06 12:59:35 -0300

    Feature: add event door #02

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <main>
       <div style={{ display: "flex" }}>
-        <Door value={d1} />
+        <Door value={d1} onChange={(newDoor) => setD1(newDoor)} />
       </div>
     </main>
   );
--- src/components/Door.tsx
+++ src/components/Door.tsx
@@ -3,13 +3,17 @@ import DoorModel from "../model/door";
 
 interface DoorProps {
   value: DoorModel;
+  onChange: (newDoor: DoorModel) => void;
 }
 
 const Door = (props: DoorProps) => {
   const door = props.value;
   const selected = door.selected ? styles.selected : "";
+
+  const toggleSelected = e => props.onChange(door.toggleSelection())
+
   return (
-    <div className={styles.area}>
+    <div className={styles.area} onClick={toggleSelected}>
       <div className={`${styles.frame} ${selected}`}>
         <div className={styles.door}>
           <div className={styles.number}>{door.number}</div>
