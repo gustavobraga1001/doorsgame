5c3af5e10d0c5e616d8a19441808af353fce0b7d
“gustavobr1001” <100322388+gustavobraga1001@users.noreply.github.com>
2024-08-13 19:35:12 -0300

    Feature: create form buttons

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
+import Card from "../components/Card";
+import styles from "../styles/Forms.module.css"
+import Link from "next/link";
+
+export default function Forms() {
   return (
-    <main>
-      <h1>Inicio do jogo</h1>
+    <main className={styles.forms}>
+      <div>
+        <Card bgColor={"#c0392c"}>
+          <h1>Monty Hall</h1>
+        </Card>
+        <Card></Card>
+      </div>
+      <div>
+        <Card ></Card>
+        <Card bgColor={"#28a085"}>
+          <Link href={`/game/10/2`} className={styles.link}>
+            <h2 >Iniciar</h2>
+          </Link>
+        </Card>
+      </div>
     </main>
   );
 }
